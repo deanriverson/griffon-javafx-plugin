82ef5790249c12585a39124325992520f5b1fab5
Dean Iverson <deanriverson@gmail.com>
2011-08-19 20:22:20 -0600

    Updated griffon version in app properties.  Finalized plugin description and added APL license text.

--- JavafxGriffonPlugin.groovy
+++ JavafxGriffonPlugin.groovy
@@ -1,20 +1,29 @@
+/*
+ * Copyright 2009-2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author Dean Iverson
+ */
 class JavafxGriffonPlugin {
-    // the plugin version
     def version = "0.1"
-    // the version or versions of Griffon the plugin is designed for
-    def griffonVersion = '0.9.3-beta-1 > *' 
-    // the other plugins this plugin depends on
-    def dependsOn = [:] //['groovyfx':'0.1']
-    // resources that are included in plugin packaging
+    def griffonVersion = '0.9.3 > *'
+    def dependsOn = ['groovyfx':'0.1']
     def pluginIncludes = []
-    // the plugin license
-    def license = '<UNKNOWN>'
-    // Toolkit compatibility. No value means compatible with all
-    // Valid values are: swing, javafx, swt, pivot, gtk
+    def license = 'Apache Public License 2.0'
     def toolkits = ['javafx']
-    // Platform compatibility. No value means compatible with all
-    // Valid values are:
-    // linux, linux64, windows, windows64, macosx, macosx64, solaris
     def platforms = []
 
     // TODO Fill in these fields
@@ -24,7 +33,5 @@ class JavafxGriffonPlugin {
     def description = '''
 This plugin allows you to write JavaFX applications using the Griffon framework.
 '''
-
-    // URL to the plugin's documentation
     def documentation = 'http://griffon.codehaus.org/Javafx+Plugin'
 }
