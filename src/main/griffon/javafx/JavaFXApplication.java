88eaddca28d7eb9514d8db4d4f2df84ad54fa7de
Danno Ferrin <danno.ferrin@shemnon.com>
2012-08-12 10:02:57 -0600

    Mane JavaFX Application Public (so it can be easily reflected)

--- src/main/griffon/javafx/JavaFXApplication.java
+++ src/main/griffon/javafx/JavaFXApplication.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2011 the original author or authors.
+ * Copyright 2009-2012 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -36,7 +36,7 @@ import static griffon.util.GriffonExceptionHandler.sanitize;
  *
  * @author Dean Iverson
  */
-class JavaFXApplication extends AbstractGriffonApplication implements StandaloneGriffonApplication {
+public class JavaFXApplication extends AbstractGriffonApplication implements StandaloneGriffonApplication {
     private static final Logger LOG = LoggerFactory.getLogger(JavaFXApplication.class);
     private boolean primaryStageDispensed = false;
     private final WindowManager windowManager;
